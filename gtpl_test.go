df0f5242bceaccaa1ed6b3ba2050361d642f2df1
Karel Kubat <karel@kubat.nl>
2023-04-02 14:12:13 +0200

    added test files

---
+++ gtpl_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func TestAll(t *testing.T) {
+	// Testing isn't implemented, the doc generation runs the tool which must succeed for a successful `make`.
+}
---
+++ syringe/syringe_test.go
@@ -0,0 +1,8 @@
+package syringe
+
+import "testing"
+
+func TestAll(t *testing.T) {
+	// Testing isn't implemented, the doc generation runs the tool which must succeed for a successful `make`.
+	// I might add specific smaller-unit tests later.
+}
