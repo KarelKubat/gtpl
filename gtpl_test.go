7bff6ff2cb277d728b6090c90bff7abddd4f1630
Karel Kubat <karel@kubat.nl>
2023-04-12 14:20:11 +0200

    tests

--- gtpl_test.go
+++ gtpl_test.go
@@ -2,6 +2,8 @@ package main
 
 import "testing"
 
-func TestAll(t *testing.T) {
-	// Testing isn't implemented, the doc generation runs the tool which must succeed for a successful `make`.
+// There isn't a lot to test in package main. All relevant tests occur in sub-packages.
+
+func TestCheck(t *testing.T) {
+	check(nil) // If this does an os.Exit(1) then the test suite will fail.
 }
